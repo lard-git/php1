c325ade075c1dc0c55528a53478a917c9d6ba2e4
lard <elizarjayarevalo@gmail.com>
2025-10-06 08:22:10 +0800

    Add files via upload
    xd

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $Name = "Arevalo, Elizar Jay";
+    $Age = "20";
+    $Color = "Violet";
+    echo "<h1>HI! my name is $Name, im $Age, and my favorite color is $Color</h1>";
+
+    $a = 10;
+    $b = 3;
+
+    echo "<p>The sum of $a and $b is $a+$b</p>" ;
+    ?>
+</body>
+</html>
